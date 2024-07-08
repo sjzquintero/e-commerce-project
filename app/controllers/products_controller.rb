d7784f5adbbc9b69a0e376b656fbc6698722a10d
Santiago Jimenez <squintero@rrc.ca>
2024-07-08 15:11:20 -0500

    pagination added to the products list by categoriesand all products list

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,11 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.page(params[:page]).per(10)
   end
 
   def by_category
     @category = Category.find(params[:id])
-    @products = @category.products
+    @products = @category.products.page(params[:page]).per(10)
   end
 
   def show
