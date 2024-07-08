e7945a880b070e18e61be383fb1a74f566866fa3
Santiago Jimenez <squintero@rrc.ca>
2024-07-07 23:50:54 -0500

    seeded with at least 100 products using faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,21 @@
 require 'faker'
 
-# Limpiar la base de datos para evitar duplicados en desarrollo
-Category.destroy_all
+# Seed categories
+categories = Category.all
 
-# Crear categorías
-categories = [
-  { name: 'T-Shirts and Tops', description: 'Comfortable and stylish urban tops.' },
-  { name: 'Jeans and Pants', description: 'Variety of jeans and pants for any occasion.' },
-  { name: 'Hoodies and Sweatshirts', description: 'Casual and trendy hoodies and sweatshirts.' },
-  { name: 'Jackets and Coats', description: 'Stylish jackets and coats for urban wear.' },
-  { name: 'Footwear', description: 'Urban shoes and sneakers.' },
-  { name: 'Accessories', description: 'Complete your look with our accessories.' }
-]
+# Define the number of products you want to seed
+number_of_products = 100
 
-# Crear las categorías en la base de datos
-categories.each do |category|
-  Category.create(category)
-end
-
-puts "Categories created successfully."
-
-# Sembrar productos para cada categoría
-categories.each do |category|
-  17.times do
-    category_record = Category.find_by(name: category[:name])
-    Product.create!(
-      name: Faker::Commerce.product_name,
-      description: Faker::Lorem.paragraph(sentence_count: 2),
-      price: Faker::Commerce.price(range: 10.0..100.0),
-      stock: Faker::Number.between(from: 1, to: 100),
-      category_id: category_record.id  # Usar category_id en lugar de category
-    )
-  end
+# Seed products
+number_of_products.times do
+  category = categories.sample  # Select a random category for each product
+  Product.create!(
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    price: Faker::Commerce.price(range: 10.0..100.0),
+    stock: Faker::Number.between(from: 1, to: 100),
+    category: category
+  )
 end
 
 puts "Seed data for products created successfully!"
